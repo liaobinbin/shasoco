12a1d8a96168f24c5c128aa28969008f0a99d019
liaobinbin <doubiliao@gmail.com>
2017-11-23 17:58:28 +0800

    delete redmine init setting

--- compose/redmine-init.sql
+++ compose/redmine-init.sql
@@ -11,20 +11,5 @@ COPY auth_sources (id, type, name, host, port, account, account_password, base_d
 -- Data for Name: settings; Type: TABLE DATA; Schema: public; Owner: redmine
 --
 
-COPY settings (id, name, value, updated_on) FROM stdin;
-1	login_required	0	2015-12-14 19:02:47.581138
-2	autologin	0	2015-12-14 19:02:47.623911
-3	self_registration	0	2015-12-14 19:02:47.637103
-4	unsubscribe	0	2015-12-14 19:02:47.647934
-5	password_min_length	8	2015-12-14 19:02:47.661978
-6	password_max_age	0	2015-12-14 19:02:47.673028
-7	lost_password	0	2015-12-14 19:02:47.68685
-8	max_additional_emails	5	2015-12-14 19:02:47.697857
-9	openid	0	2015-12-14 19:02:47.711733
-10	rest_api_enabled	0	2015-12-14 19:02:47.72272
-11	jsonp_enabled	0	2015-12-14 19:02:47.73673
-12	session_lifetime	0	2015-12-14 19:02:47.747721
-13	session_timeout	0	2015-12-14 19:02:47.761951
-\.
 
 
